2dd2469a26ad7f369e4ce1cdecd0b660b721a5c8
Maikel Bos <maikel.bos0@gmail.com>
2023-08-30 19:18:45 +0200

    Add more specific change checking methods since references will always be unequal

--- src/VDT.Core.Blazor.XYChart.Tests/ChildComponentBaseTests.cs
+++ src/VDT.Core.Blazor.XYChart.Tests/ChildComponentBaseTests.cs
@@ -6,7 +6,8 @@ namespace VDT.Core.Blazor.XYChart.Tests;
 
 public class ChildComponentBaseTests {
     private class TestComponent : ChildComponentBase {
-        public bool? Value { get; set; }
+        public int Struct { get; set; }
+        public string? String { get; set; }
 
         public override bool HaveParametersChanged(ParameterView parameters) {
             throw new System.NotImplementedException();
@@ -14,33 +15,54 @@ public class ChildComponentBaseTests {
     }
 
     [Theory]
-    [InlineData(null, null, false)]
-    [InlineData(false, false, false)]
-    [InlineData(true, true, false)]
-    [InlineData(null, false, true)]
-    [InlineData(null, true, true)]
-    [InlineData(false, null, true)]
-    [InlineData(true, null, true)]
-    [InlineData(true, false, true)]
-    [InlineData(false, true, true)]
-    public void HasParameterChanged(bool? oldValue, bool? newValue, bool expectedResult) {
+    [InlineData(1, 1, false)]
+    [InlineData(1, null, false)]
+    [InlineData(1, 2, true)]
+    public void HasParameterChanged_Struct(int oldValue, int? newValue, bool expectedResult) {
         var subject = new TestComponent() {
-            Value = oldValue
+            Struct = oldValue
         };
 
         var parameters = ParameterView.FromDictionary(new Dictionary<string, object?>() {
-            { nameof(TestComponent.Value), newValue }
+            { nameof(TestComponent.Struct), newValue }
         });
 
-        Assert.Equal(expectedResult, subject.HasParameterChanged(parameters, nameof(subject.Value), subject.Value));
+        Assert.Equal(expectedResult, ChildComponentBase.HasParameterChanged(parameters, nameof(subject.Struct), subject.Struct));
     }
 
     [Fact]
-    public void HasParameterChanged_NotPresentInParameters() {
+    public void HasParameterChanged_Struct_NotPresentInParameters() {
         var subject = new TestComponent();
 
         var parameters = ParameterView.FromDictionary(new Dictionary<string, object?>());
 
-        Assert.False(subject.HasParameterChanged(parameters, nameof(subject.Value), subject.Value));
+        Assert.False(ChildComponentBase.HasParameterChanged(parameters, nameof(subject.Struct), subject.Struct));
+    }
+
+    [Theory]
+    [InlineData(null, null, false)]
+    [InlineData("foo", "foo", false)]
+    [InlineData("foo", "bar", true)]
+    [InlineData("foo", null, true)]
+    [InlineData(null, "foo", true)]
+    public void HasParameterChanged_String(string? oldValue, string? newValue, bool expectedResult) {
+        var subject = new TestComponent() {
+            String = oldValue
+        };
+
+        var parameters = ParameterView.FromDictionary(new Dictionary<string, object?>() {
+            { nameof(TestComponent.String), newValue }
+        });
+
+        Assert.Equal(expectedResult, ChildComponentBase.HasParameterChanged(parameters, nameof(subject.String), subject.String));
+    }
+
+    [Fact]
+    public void HasParameterChanged_String_NotPresentInParameters() {
+        var subject = new TestComponent();
+
+        var parameters = ParameterView.FromDictionary(new Dictionary<string, object?>());
+
+        Assert.False(ChildComponentBase.HasParameterChanged(parameters, nameof(subject.String), subject.String));
     }
 }
--- src/VDT.Core.Blazor.XYChart/ChildComponentBase.cs
+++ src/VDT.Core.Blazor.XYChart/ChildComponentBase.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
@@ -20,7 +21,13 @@ public abstract class ChildComponentBase : ComponentBase {
     
     public abstract bool HaveParametersChanged(ParameterView parameters);
 
-    public bool HasParameterChanged<T>(ParameterView parameters, string parameterName, T oldValue) {
-        return parameters.TryGetValue(parameterName, out T? value) && !EqualityComparer<T>.Default.Equals(value, oldValue);
+    public static bool HasParameterChanged(ParameterView parameters, string parameterName, string? oldValue) {
+        return parameters.TryGetValue(parameterName, out string? value) && !string.Equals(value, oldValue);
+    }
+
+    public static bool HasParameterChanged<T>(ParameterView parameters, string parameterName, T oldValue) where T : struct {
+        return parameters.TryGetValue(parameterName, out T? value) && value != null && !value.Value.Equals(oldValue);
+    }
+
     }
 }
