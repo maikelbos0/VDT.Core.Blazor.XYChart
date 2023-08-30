1dd16b4879f7f7189dfc5c6dde52181f87882046
Maikel Bos <maikel.bos0@gmail.com>
2023-08-30 16:46:55 +0200

    Add child component base class to handle parameter change

---
+++ src/VDT.Core.Blazor.XYChart.Tests/ChildComponentBaseTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using Xunit;
+
+namespace VDT.Core.Blazor.XYChart.Tests;
+
+public class ChildComponentBaseTests {
+    private class TestComponent : ChildComponentBase {
+        public bool? Value { get; set; }
+
+        public override bool HaveParametersChanged(ParameterView parameters) {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    [Theory]
+    [InlineData(null, null, false)]
+    [InlineData(false, false, false)]
+    [InlineData(true, true, false)]
+    [InlineData(null, false, true)]
+    [InlineData(null, true, true)]
+    [InlineData(false, null, true)]
+    [InlineData(true, null, true)]
+    [InlineData(true, false, true)]
+    [InlineData(false, true, true)]
+    public void HasParameterChanged(bool? oldValue, bool? newValue, bool expectedResult) {
+        var subject = new TestComponent() {
+            Value = oldValue
+        };
+
+        var parameters = ParameterView.FromDictionary(new Dictionary<string, object?>() {
+            { nameof(TestComponent.Value), newValue }
+        });
+
+        Assert.Equal(expectedResult, subject.HasParameterChanged(parameters, nameof(subject.Value), subject.Value));
+    }
+
+    [Fact]
+    public void HasParameterChanged_NotPresentInParameters() {
+        var subject = new TestComponent();
+
+        var parameters = ParameterView.FromDictionary(new Dictionary<string, object?>());
+
+        Assert.False(subject.HasParameterChanged(parameters, nameof(subject.Value), subject.Value));
+    }
+}
---
+++ src/VDT.Core.Blazor.XYChart/ChildComponentBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace VDT.Core.Blazor.XYChart;
+
+public abstract class ChildComponentBase : ComponentBase {
+    [CascadingParameter] internal XYChart Chart { get; set; } = null!;
+
+    public override async Task SetParametersAsync(ParameterView parameters) {
+        var parametersHaveChanged = HaveParametersChanged(parameters);
+
+        await base.SetParametersAsync(parameters);
+
+        if (parametersHaveChanged) {
+            Chart.HandleStateChange();
+        }
+    }
+    
+    public abstract bool HaveParametersChanged(ParameterView parameters);
+
+    public bool HasParameterChanged<T>(ParameterView parameters, string parameterName, T oldValue) {
+        return parameters.TryGetValue(parameterName, out T? value) && !EqualityComparer<T>.Default.Equals(value, oldValue);
+    }
+}
