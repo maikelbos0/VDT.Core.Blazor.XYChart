824cee012db59273b45b4ca3370805c8c35a34fe
Maikel Bos <maikel.bos0@gmail.com>
2023-09-09 12:59:39 +0200

    Revert path closing; it will not make sense when fixing stacked area

--- src/VDT.Core.Blazor.XYChart/Shapes/PathCommandFactory.cs
+++ src/VDT.Core.Blazor.XYChart/Shapes/PathCommandFactory.cs
@@ -3,6 +3,8 @@
 namespace VDT.Core.Blazor.XYChart.Shapes;
 
 public static class PathCommandFactory {
+    public const string ClosePath = "Z";
+
     public static string MoveTo(decimal x, decimal y) => FormattableString.Invariant($"M {x} {y}");
 
     public static string LineTo(decimal x, decimal y) => FormattableString.Invariant($"L {x} {y}");
