626f149434bace74d804da5fe64111c9c91c32d0
Maikel Bos <maikel.bos0@gmail.com>
2023-09-19 20:31:35 +0200

    Remove redundant tests that are covered by shape test

--- src/VDT.Core.Blazor.XYChart.Tests/CanvasTests.cs
+++ src/VDT.Core.Blazor.XYChart.Tests/CanvasTests.cs
@@ -45,66 +45,6 @@ public class CanvasTests {
     }
 
     [Fact]
-    public void PlotAreaX() {
-        var subject = new Canvas() {
-            Width = 1000,
-            Height = 500,
-            Padding = 25,
-            XAxisLabelHeight = 50,
-            XAxisLabelClearance = 5,
-            YAxisLabelWidth = 75,
-            YAxisLabelClearance = 10
-        };
-
-        Assert.Equal(25 + 75, subject.PlotAreaX);
-    }
-
-    [Fact]
-    public void PlotAreaY() {
-        var subject = new Canvas() {
-            Width = 1000,
-            Height = 500,
-            Padding = 25,
-            XAxisLabelHeight = 50,
-            XAxisLabelClearance = 5,
-            YAxisLabelWidth = 75,
-            YAxisLabelClearance = 10
-        };
-
-        Assert.Equal(25, subject.PlotAreaY);
-    }
-
-    [Fact]
-    public void PlotAreaWidth() {
-        var subject = new Canvas() {
-            Width = 1000,
-            Height = 500,
-            Padding = 25,
-            XAxisLabelHeight = 50,
-            XAxisLabelClearance = 5,
-            YAxisLabelWidth = 75,
-            YAxisLabelClearance = 10
-        };
-
-        Assert.Equal(1000 - 25 - 25 - 75, subject.PlotAreaWidth);
-    }
-
-    [Fact]
-    public void PlotAreaHeight() {
-        var subject = new Canvas() {
-            Width = 1000,
-            Height = 500,
-            Padding = 25,
-            XAxisLabelHeight = 50,
-            XAxisLabelClearance = 5,
-            YAxisLabelWidth = 75,
-            YAxisLabelClearance = 10
-        };
-
-        Assert.Equal(500 - 25 - 25 - 50, subject.PlotAreaHeight);
-    }
-
-    [Fact]
     public void GetPlotAreaShape() {
         var subject = new Canvas() {
             Width = 1000,
