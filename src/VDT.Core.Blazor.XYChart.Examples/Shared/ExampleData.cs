c703ac940cb099416a1e7d571731628feb788125
Maikel Bos <maikel.bos0@gmail.com>
2023-10-03 17:07:18 +0200

    Add more attributes to example data

--- src/VDT.Core.Blazor.XYChart.Examples/Shared/ExampleData.cs
+++ src/VDT.Core.Blazor.XYChart.Examples/Shared/ExampleData.cs
@@ -1,17 +1,28 @@
 ﻿namespace VDT.Core.Blazor.XYChart.Examples.Shared;
 
-public static class ExampleData {
+public class ExampleData {
     public static List<string> Labels => new() { "Foo", "Bar", "Baz", "Quux", "Quuux" };
-    public static List<List<decimal?>> BarDataSeries = new() {
-        new() { 11000, 19000, 31500, -2500, 9500 },
-        new() { 21000, -12000, 15500, -4000, 3500 }
+
+    public static List<ExampleData> BarData = new() {
+        new("Bar", "#f1c40f", 11000, 19000, 31500, -2500, 9500),
+        new("Baz", "#00cc66", 21000, -12000, 15500, -4000, 3500)
     };
-    public static List<List<decimal?>> LineDataSeries = new() {
-        new() { 12500, 9500, 20500, 14500, 11000 },
-        new() { 6500, -2500, null, 4000, 2000 }
+    public static List<ExampleData> LineData = new() {
+        new("Line", "#2274a5", 12500, 9500, 20500, 14500, 11000),
+        new("Linz", "#d90368", 6500, -2500, null, 4000, 2000)
     };
-    public static List<List<decimal?>> AreaDataSeries = new() {
-        new() { 7000, 12500, 8500, 3500, 11000 },
-        new() { 2500, -2500, 3500, 5000, 3000 }
+    public static List<ExampleData> AreaData = new() {
+        new("Area", "#ff9933", 7000, 12500, 8500, 3500, 11000),
+        new("Arez", "#cc99ff", 2500, -2500, 3500, 5000, 3000)
     };
+
+    public string Name { get; }
+    public string Color { get; }
+    public List<decimal?> DataPoints { get; }
+
+    public ExampleData(string name, string color, params decimal?[] dataPoints) {
+        Name = name;
+        Color = color;
+        DataPoints = dataPoints.ToList(); // TODO?
+    }
 }
