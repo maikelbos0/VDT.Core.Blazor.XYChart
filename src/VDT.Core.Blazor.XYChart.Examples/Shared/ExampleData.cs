5d367a4de7d06ec7eed8c46c74241444e9cc78f5
Maikel Bos <maikel.bos0@gmail.com>
2023-10-03 16:17:22 +0200

    Centralize example data

---
+++ src/VDT.Core.Blazor.XYChart.Examples/Shared/ExampleData.cs
@@ -0,0 +1,13 @@
+﻿namespace VDT.Core.Blazor.XYChart.Examples.Shared;
+
+public static class ExampleData {
+    public static List<string> Labels => new() { "Foo", "Bar", "Baz", "Quux", "Quuux" };
+    public static List<List<decimal?>> BarDataSeries = new() {
+        new() { 11000, 19000, 31500, -2500, 9500 },
+        new() { 21000, -12000, 15500, -4000, 3500 }
+    };
+    public static List<List<decimal?>> LineDataSeries = new() {
+        new() { 12500, 9500, 20500, 14500, 11000 },
+        new() { 6500, -2500, null, 4000, 2000 }
+    };
+}
