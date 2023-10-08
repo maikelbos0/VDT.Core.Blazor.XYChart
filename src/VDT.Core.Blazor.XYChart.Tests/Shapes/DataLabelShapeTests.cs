6503ffe19f183f9dc96f867dedddf472a1ddd48a
Maikel Bos <maikel.bos0@gmail.com>
2023-10-08 09:40:35 +0200

    Add data label shape type

---
+++ src/VDT.Core.Blazor.XYChart.Tests/Shapes/DataLabelShapeTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using VDT.Core.Blazor.XYChart.Shapes;
+using Xunit;
+
+namespace VDT.Core.Blazor.XYChart.Tests.Shapes;
+
+public class DataLabelShapeTests {
+    [Fact]
+    public void Key() {
+        var subject = new DataLabelShape(100M, 50M, "150", "example-data", 1, 2, 5);
+
+        Assert.Equal("DataLabelShape[1,2,5]", subject.Key);
+    }
+
+    [Fact]
+    public void GetAttributes() {
+        var subject = new DataLabelShape(100M, 50M, "150", "example-data", 1, 2, 5);
+
+        var result = subject.GetAttributes();
+
+        Assert.Equal(2, result.Count());
+        Assert.Equal("100", Assert.Single(result, attribute => attribute.Key == "x").Value);
+        Assert.Equal("50", Assert.Single(result, attribute => attribute.Key == "y").Value);
+    }
+
+    [Fact]
+    public void GetContent() {
+        var subject = new DataLabelShape(100M, 50M, "150", "example-data", 1, 2, 5);
+
+        Assert.Equal("150", subject.GetContent());
+    }
+}
---
+++ src/VDT.Core.Blazor.XYChart/Shapes/DataLabelShape.cs
@@ -0,0 +1,25 @@
+﻿namespace VDT.Core.Blazor.XYChart.Shapes;
+
+public class DataLabelShape : ShapeBase {
+    public const string DefaultCssClass = "data-label";
+
+    public override string ElementName => "text";
+    public decimal X { get; }
+    public decimal Y { get; }
+    public string Value { get; }
+    public override string CssClass { get; }
+
+    public DataLabelShape(decimal x, decimal y, string value, string? cssClass, int layerIndex, int dataSeriesIndex, int dataPointIndex) : base(layerIndex, dataSeriesIndex, dataPointIndex) {
+        Y = y;
+        X = x;
+        Value = value;
+        CssClass = $"{DefaultCssClass} {cssClass}";
+    }
+
+    public override ShapeAttributeCollection GetAttributes() => new() {
+        { "x", X },
+        { "y", Y }
+    };
+
+    public override string? GetContent() => Value;
+}
