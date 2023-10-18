4bc69577b3f0139c37fc179f4045ce91fb8d67e0
Maikel Bos <maikel.bos0@gmail.com>
2023-10-18 19:39:25 +0200

    Add shape for legend key

---
+++ src/VDT.Core.Blazor.XYChart.Tests/Shapes/LegendKeyShapeTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using VDT.Core.Blazor.XYChart.Shapes;
+using Xunit;
+
+namespace VDT.Core.Blazor.XYChart.Tests.Shapes;
+
+public class LegendKeyShapeTests {
+    [Fact]
+    public void Key() {
+        var subject = new LegendKeyShape(20, 50, 80, 90, "red", "example-data", 1, 2);
+
+        Assert.Equal($"{nameof(LegendKeyShape)}[1,2]", subject.Key);
+    }
+
+    [Fact]
+    public void GetAttributes() {
+        var subject = new LegendKeyShape(20, 50, 80, 90, "red", "example-data", 1, 2);
+
+        var result = subject.GetAttributes();
+
+        Assert.Equal(5, result.Count());
+        Assert.Equal("20", Assert.Single(result, attribute => attribute.Key == "x").Value);
+        Assert.Equal("50", Assert.Single(result, attribute => attribute.Key == "y").Value);
+        Assert.Equal("80", Assert.Single(result, attribute => attribute.Key == "width").Value);
+        Assert.Equal("90", Assert.Single(result, attribute => attribute.Key == "height").Value);
+        Assert.Equal("red", Assert.Single(result, attribute => attribute.Key == "fill").Value);
+    }
+}
---
+++ src/VDT.Core.Blazor.XYChart/Shapes/LegendKeyShape.cs
@@ -0,0 +1,32 @@
+﻿using VDT.Core.Blazor.XYChart.Shapes;
+
+namespace VDT.Core.Blazor.XYChart.Shapes;
+
+public class LegendKeyShape : ShapeBase {
+    public const string DefaultCssClass = "legend-key";
+
+    public override string ElementName => "rect";
+    public decimal X { get; }
+    public decimal Y { get; }
+    public decimal Width { get; }
+    public decimal Height { get; }
+    public string Color { get; }
+    public override string CssClass { get; }
+
+    public LegendKeyShape(decimal x, decimal y, decimal width, decimal height, string color, string? cssClass, int layerIndex, int dataSeriesIndex) : base(layerIndex, dataSeriesIndex) {
+        Y = y;
+        X = x;
+        Height = height;
+        Width = width;
+        Color = color;
+        CssClass = $"{DefaultCssClass} {cssClass}";
+    }
+
+    public override ShapeAttributeCollection GetAttributes() => new() {
+        { "x", X },
+        { "y", Y },
+        { "width", Width },
+        { "height", Height },
+        { "fill", Color }
+    };
+}
