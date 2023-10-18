361b4a641026fcc75f10986562d4b03d615bad09
Maikel Bos <maikel.bos0@gmail.com>
2023-10-18 19:45:46 +0200

    Add shape for legend text

---
+++ src/VDT.Core.Blazor.XYChart.Tests/Shapes/LegendTextShapeTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using VDT.Core.Blazor.XYChart.Shapes;
+using Xunit;
+
+namespace VDT.Core.Blazor.XYChart.Tests.Shapes;
+
+public class LegendTextShapeTests {
+    [Fact]
+    public void Key() {
+        var subject = new LegendTextShape(100M, 50M, "Foo", "example-data", 1, 2);
+
+        Assert.Equal($"{nameof(LegendTextShape)}[1,2]", subject.Key);
+    }
+
+    [Fact]
+    public void GetAttributes() {
+        var subject = new LegendTextShape(100M, 50M, "Foo", "example-data", 1, 2);
+
+        var result = subject.GetAttributes();
+
+        Assert.Equal(2, result.Count());
+        Assert.Equal("100", Assert.Single(result, attribute => attribute.Key == "x").Value);
+        Assert.Equal("50", Assert.Single(result, attribute => attribute.Key == "y").Value);
+    }
+
+    [Fact]
+    public void GetContent() {
+        var subject = new LegendTextShape(100M, 50M, "Foo", "example-data", 1, 2);
+
+        Assert.Equal("Foo", subject.GetContent());
+    }
+}
---
+++ src/VDT.Core.Blazor.XYChart/Shapes/LegendTextShape.cs
@@ -0,0 +1,28 @@
+﻿using VDT.Core.Blazor.XYChart.Shapes;
+
+namespace VDT.Core.Blazor.XYChart.Shapes;
+
+public class LegendTextShape : ShapeBase {
+    public const string DefaultCssClass = "legend-text";
+
+    public override string ElementName => "text";
+    public decimal X { get; }
+    public decimal Y { get; }
+    public string DataSeriesName { get; }
+    public override string CssClass { get; }
+
+    public LegendTextShape(decimal x, decimal y, string dataSeriesName, string? cssClass, int layerIndex, int dataSeriesIndex) : base(layerIndex, dataSeriesIndex) {
+
+        CssClass = $"{DefaultCssClass} {cssClass}";
+        X = x;
+        Y = y;
+        DataSeriesName = dataSeriesName;
+    }
+
+    public override ShapeAttributeCollection GetAttributes() => new() {
+        { "x", X },
+        { "y", Y }
+    };
+
+    public override string? GetContent() => DataSeriesName;
+}
