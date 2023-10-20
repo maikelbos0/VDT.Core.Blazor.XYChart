b015de6db538844b5851b1719914356cb3495ba2
Maikel Bos <maikel.bos0@gmail.com>
2023-10-20 16:46:10 +0200

    Fix legend defaults

--- src/VDT.Core.Blazor.XYChart/Legend.cs
+++ src/VDT.Core.Blazor.XYChart/Legend.cs
@@ -4,12 +4,12 @@ using System;
 namespace VDT.Core.Blazor.XYChart;
 
 public class Legend : ChildComponentBase, IDisposable {
-    public static bool DefaultIsEnabled { get; set; } = false;
+    public static bool DefaultIsEnabled { get; set; } = true;
     public static LegendPosition DefaultPosition { get; set; } = LegendPosition.Top;
     public static LegendAlignment DefaultAlignment { get; set; } = LegendAlignment.Center;
     public static int DefaultHeight { get; set; } = 25;
-    public static int DefaultItemWidth { get; set; } = 25;
-    public static int DefaultItemHeight { get; set; } = 1000;
+    public static int DefaultItemWidth { get; set; } = 100;
+    public static int DefaultItemHeight { get; set; } = 25;
     public static int DefaultKeySize { get; set; } = 16;
 
     [Parameter] public bool IsEnabled { get; set; } = DefaultIsEnabled;
