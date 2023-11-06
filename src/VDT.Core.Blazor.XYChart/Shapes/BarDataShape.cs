0380e62ceccfb5ee7a0884101e1d0e864cb25df1
Maikel Bos <maikel.bos0@gmail.com>
2023-11-06 20:44:51 +0100

    Add bar data shape docs

--- src/VDT.Core.Blazor.XYChart/Shapes/BarDataShape.cs
+++ src/VDT.Core.Blazor.XYChart/Shapes/BarDataShape.cs
@@ -2,17 +2,58 @@
 
 namespace VDT.Core.Blazor.XYChart.Shapes;
 
+/// <summary>
+/// SVG shape to display a data point in a bar data series
+/// </summary>
 public class BarDataShape : ShapeBase {
+    /// <summary>
+    /// Default CSS classes that are always applied to this SVG shape
+    /// </summary>
     public const string DefaultCssClass = "data bar-data";
-
+    
+    /// <inheritdoc/>
     public override string ElementName => "rect";
+
+    /// <summary>
+    /// Gets the X coordinate of the bar
+    /// </summary>
     public decimal X { get; }
+
+    /// <summary>
+    /// Gets the Y coordinate of the bar; if <see cref="Height"/> is negative it will be added to the resulting attribute
+    /// </summary>
     public decimal Y { get; }
+
+    /// <summary>
+    /// Gets the width of the bar
+    /// </summary>
     public decimal Width { get; }
+
+    /// <summary>
+    /// Gets the height of the bar; if negative it will be negated
+    /// </summary>
     public decimal Height { get; }
+
+    /// <summary>
+    /// Gets the data series color
+    /// </summary>
     public string Color { get; }
+
+    /// <inheritdoc/>
     public override string CssClass { get; }
 
+    /// <summary>
+    /// Creates an SVG bar data shape
+    /// </summary>
+    /// <param name="x">X coordinate of the bar</param>
+    /// <param name="y">Y coordinate of the bar</param>
+    /// <param name="width">Width of the bar</param>
+    /// <param name="height">Height of the bar</param>
+    /// <param name="color">Data series color</param>
+    /// <param name="cssClass">Data series CSS class</param>
+    /// <param name="layerIndex">Index of the layer that contains the data series</param>
+    /// <param name="dataSeriesIndex">Index of the data series in the containing layer</param>
+    /// <param name="dataPointIndex">Index of the data point in the data series</param>
     public BarDataShape(decimal x, decimal y, decimal width, decimal height, string color, string? cssClass, int layerIndex, int dataSeriesIndex, int dataPointIndex) : base(layerIndex, dataSeriesIndex, dataPointIndex) {
         Y = y;
         X = x;
@@ -22,6 +63,7 @@ public class BarDataShape : ShapeBase {
         CssClass = $"{DefaultCssClass} {cssClass}";
     }
 
+    /// <inheritdoc/>
     public override ShapeAttributeCollection GetAttributes() => new() {
         { "x", X },
         { "y", Height < 0 ? Y + Height : Y },
