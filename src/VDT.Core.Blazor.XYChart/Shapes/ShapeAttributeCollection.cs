32e19eeb7f971421aad97e87eb7f8f3ca82b8c2f
Maikel Bos <maikel.bos0@gmail.com>
2023-11-08 20:02:35 +0100

    Add doc comments to shape helper classes

--- src/VDT.Core.Blazor.XYChart/Shapes/ShapeAttributeCollection.cs
+++ src/VDT.Core.Blazor.XYChart/Shapes/ShapeAttributeCollection.cs
@@ -4,14 +4,29 @@ using System.Globalization;
 
 namespace VDT.Core.Blazor.XYChart.Shapes;
 
+/// <summary>
+/// Collection of key-value pairs representing attributes
+/// </summary>
 public class ShapeAttributeCollection : IEnumerable<KeyValuePair<string, object>> {
     private readonly Dictionary<string, object> attributes = new();
 
+    /// <summary>
+    /// Add an attribute
+    /// </summary>
+    /// <param name="key">Attribute name</param>
+    /// <param name="value">Attribute value</param>
     public void Add(string key, decimal value) => attributes.Add(key, DecimalMath.Trim(value).ToString(CultureInfo.InvariantCulture));
 
+    /// <summary>
+    /// Add an attribute
+    /// </summary>
+    /// <param name="key">Attribute name</param>
+    /// <param name="value">Attribute value</param>
     public void Add(string key, object value) => attributes.Add(key, value);
 
+    /// <inheritdoc/>
     public IEnumerator<KeyValuePair<string, object>> GetEnumerator() => attributes.GetEnumerator();
 
+    /// <inheritdoc/>
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
