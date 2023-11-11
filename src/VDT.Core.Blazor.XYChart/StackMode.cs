694f217ba3c1ace8be09fcdfd583d8386daa3aab
Maikel Bos <maikel.bos0@gmail.com>
2023-11-11 08:55:31 +0100

    Add stack mode doc comments

--- src/VDT.Core.Blazor.XYChart/StackMode.cs
+++ src/VDT.Core.Blazor.XYChart/StackMode.cs
@@ -1,6 +1,16 @@
 ﻿namespace VDT.Core.Blazor.XYChart;
 
+/// <summary>
+/// Determines the way data points are stacked
+/// </summary>
 public enum StackMode {
+    /// <summary>
+    /// Positive and negative data points are stacked together
+    /// </summary>
     Single,
+
+    /// <summary>
+    /// Positive data points are stacked separately from negative data points
+    /// </summary>
     Split
 }
