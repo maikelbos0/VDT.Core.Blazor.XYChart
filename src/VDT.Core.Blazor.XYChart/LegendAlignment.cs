4c424262d579912377e72988edf4afa1c3ae8fc7
Maikel Bos <maikel.bos0@gmail.com>
2023-11-12 09:04:43 +0100

    Add doc comments to legend enums

--- src/VDT.Core.Blazor.XYChart/LegendAlignment.cs
+++ src/VDT.Core.Blazor.XYChart/LegendAlignment.cs
@@ -1,7 +1,21 @@
 ﻿namespace VDT.Core.Blazor.XYChart;
 
+/// <summary>
+/// Aligns the legend items inside the legend area
+/// </summary>
 public enum LegendAlignment {
+    /// <summary>
+    /// Align left
+    /// </summary>
     Left,
+
+    /// <summary>
+    /// Center
+    /// </summary>
     Center,
+
+    /// <summary>
+    /// Align right
+    /// </summary>
     Right
 }
