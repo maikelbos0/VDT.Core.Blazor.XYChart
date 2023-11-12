75a3e3339fd4b23c5e2fd915b3e2007ac983ad46
Maikel Bos <maikel.bos0@gmail.com>
2023-11-12 09:06:04 +0100

    Add doc comments to bar layer

--- src/VDT.Core.Blazor.XYChart/BarLayer.cs
+++ src/VDT.Core.Blazor.XYChart/BarLayer.cs
@@ -5,23 +5,51 @@ using VDT.Core.Blazor.XYChart.Shapes;
 
 namespace VDT.Core.Blazor.XYChart;
 
+/// <summary>
+/// Layer in which the data is displayed as vertical bars which have a height corresponding to the data values
+/// </summary>
 public class BarLayer : LayerBase {
+    /// <summary>
+    /// Gets or sets the default value for the amount of white space on either side of the group of bars for an index, expressed as a percentage of the total
+    /// amount of space available for this index
+    /// </summary>
     public static decimal DefaultClearancePercentage { get; set; } = 10M;
+
+    /// <summary>
+    /// Gets or sets the default value for the amount of white space between each bar for an index, expressed as a percentage of the total amount of space 
+    /// available for this index; this value is ignored when the bar layer is stacked
+    /// </summary>
     public static decimal DefaultGapPercentage { get; set; } = 5M;
 
+    /// <inheritdoc/>
     public override StackMode StackMode => StackMode.Split;
+
+    /// <inheritdoc/>
     public override DataPointSpacingMode DefaultDataPointSpacingMode => DataPointSpacingMode.Center;
+
+    /// <inheritdoc/>
     public override bool NullAsZero => false;
 
+    /// <summary>
+    /// Gets or sets the amount of white space on either side of the group of bars for an index, expressed as a percentage of the total amount of space 
+    /// available for this index
+    /// </summary>
     [Parameter] public decimal ClearancePercentage { get; set; } = DefaultClearancePercentage;
+
+    /// <summary>
+    /// Gets or sets the amount of white space between each bar for an index, expressed as a percentage of the total amount of space available for this index;
+    /// this value is ignored when the bar layer is stacked
+    /// </summary>
     [Parameter] public decimal GapPercentage { get; set; } = DefaultGapPercentage;
 
+    /// <inheritdoc/>
     public override bool HaveParametersChanged(ParameterView parameters)
         => parameters.HasParameterChanged(IsStacked)
         || parameters.HasParameterChanged(ShowDataLabels)
         || parameters.HasParameterChanged(ClearancePercentage)
         || parameters.HasParameterChanged(GapPercentage);
 
+    /// <inheritdoc/>
     public override IEnumerable<ShapeBase> GetDataSeriesShapes() {
         var layerIndex = Chart.Layers.IndexOf(this);
 
@@ -42,6 +70,7 @@ public class BarLayer : LayerBase {
         }
     }
 
+    /// <inheritdoc/>
     public override IEnumerable<CanvasDataSeries> GetCanvasDataSeries() {
         var dataPointTransformer = GetDataPointTransformer();
         var width = Chart.GetDataPointWidth() / 100M * (100M - ClearancePercentage * 2);
@@ -69,5 +98,4 @@ public class BarLayer : LayerBase {
             )).ToList())
         ).ToList();
     }
-
 }
