605755d8957e4319c3b753995e078e9f0c837f28
Maikel Bos <maikel.bos0@gmail.com>
2023-11-15 19:47:15 +0100

    Add doc comments to legend item record

--- src/VDT.Core.Blazor.XYChart/LegendItem.cs
+++ src/VDT.Core.Blazor.XYChart/LegendItem.cs
@@ -1,3 +1,11 @@
 ﻿namespace VDT.Core.Blazor.XYChart;
 
+/// <summary>
+/// Information for displaying a legend item on a canvas
+/// </summary>
+/// <param name="Color">Data series color</param>
+/// <param name="Text">Data series name</param>
+/// <param name="CssClass">Data series CSS class</param>
+/// <param name="LayerIndex">Index of the layer that contains the data series</param>
+/// <param name="DataSeriesIndex">Index of the data series in the containing layer</param>
 public record class LegendItem(string Color, string Text, string? CssClass, int LayerIndex, int DataSeriesIndex);
