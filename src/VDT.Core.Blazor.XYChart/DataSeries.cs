eba71222c5a0c6edbcd9bf47d02bd281a4e8f9b9
Maikel Bos <maikel.bos0@gmail.com>
2023-11-15 19:41:15 +0100

    Add doc comments to data series

--- src/VDT.Core.Blazor.XYChart/DataSeries.cs
+++ src/VDT.Core.Blazor.XYChart/DataSeries.cs
@@ -5,9 +5,19 @@ using System.Linq;
 
 namespace VDT.Core.Blazor.XYChart;
 
+/// <summary>
+/// Series of data points to be displayed in a layer in the <see cref="XYChart"/>
+/// </summary>
 public class DataSeries : ChildComponentBase, IDisposable {
+    /// <summary>
+    /// Color that is applied to data series if no <see cref="Color"/> was provided and <see cref="DefaultColors"/> is empty
+    /// </summary>
     public const string FallbackColor = "#000000";
 
+    /// <summary>
+    /// Gets or sets the default colors for data series without a <see cref="Color"/>; the color is chosen by determining the data series index within the 
+    /// entire <see cref="XYChart"/>, using a modulus if required
+    /// </summary>
     public static List<string> DefaultColors { get; set; } = new() {
         "#ff9933",
         "#11bbdd",
@@ -20,24 +30,47 @@ public class DataSeries : ChildComponentBase, IDisposable {
     };
 
     [CascadingParameter] internal LayerBase Layer { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the name of the data series to be displayed in the legend
+    /// </summary>
     [Parameter] public string? Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the color to be used for shapes representing this data series
+    /// </summary>
     [Parameter] public string? Color { get; set; }
+
+    /// <summary>
+    /// Gets or sets the data point values that make up this data series; data points are matched to category labels in the chart by index
+    /// </summary>
     [Parameter] public IList<decimal?> DataPoints { get; set; } = new List<decimal?>();
+
+    /// <summary>
+    /// Gets or sets the CSS class applied to shapes for this data series
+    /// </summary>
     [Parameter] public string? CssClass { get; set; }
 
+    /// <inheritdoc/>
     protected override void OnInitialized() => Layer.AddDataSeries(this);
 
+    /// <inheritdoc/>
     public override bool HaveParametersChanged(ParameterView parameters)
         => parameters.HasParameterChanged(Name)
         || parameters.HasParameterChanged(Color)
         || parameters.HasParameterChanged(DataPoints)
         || parameters.HasParameterChanged(CssClass);
 
+    /// <inheritdoc/>
     public void Dispose() {
         Layer.RemoveDataSeries(this);
         GC.SuppressFinalize(this);
     }
 
+    /// <summary>
+    /// Gets the value and index of data points that should be rendered for this data series, taking into account the category label count and layer settings
+    /// </summary>
+    /// <returns>A collection of data point values and indexes</returns>
     public IEnumerable<(int Index, decimal DataPoint)> GetDataPoints() {
         var dataPoints = DataPoints.Take(Chart.Labels.Count);
 
@@ -56,6 +89,10 @@ public class DataSeries : ChildComponentBase, IDisposable {
         }
     }
 
+    /// <summary>
+    /// Determines the color to be used for this data series based on <see cref="Color"/>, <see cref="DefaultColors"/> and <see cref="FallbackColor"/>
+    /// </summary>
+    /// <returns>The data series color</returns>
     public string GetColor() {
         if (Color != null) {
             return Color;
