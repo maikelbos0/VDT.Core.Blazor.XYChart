8c656c4f457d37370fc708afd33025e66fa36e03
Maikel Bos <maikel.bos0@gmail.com>
2024-01-03 19:45:56 +0100

    Add text size implementation

--- src/VDT.Core.Blazor.XYChart/XYChart.cs
+++ src/VDT.Core.Blazor.XYChart/XYChart.cs
@@ -13,7 +13,7 @@ namespace VDT.Core.Blazor.XYChart;
 /// Component to render charts with a category X-axis and a value Y-axis
 /// </summary>
 public class XYChart : ComponentBase, IAsyncDisposable {
-    internal const string ModuleLocation = "./_content/VDT.Core.Blazor.XYChart/xychart.be75333fd4.js";
+    internal const string ModuleLocation = "./_content/VDT.Core.Blazor.XYChart/xychart.bfc42c57b9.js";
 
     /// <summary>
     /// Gets or sets the default value for the the way data points are spaced out over the plot area
--- src/VDT.Core.Blazor.XYChart/wwwroot/xychart.be75333fd4.js
+++
@@ -1,9 +0,0 @@
-
-function getTextSize(text, cssClass) {
-    return {
-        width: 50,
-        height: 25
-    };
-}
-
-export { getTextSize };
