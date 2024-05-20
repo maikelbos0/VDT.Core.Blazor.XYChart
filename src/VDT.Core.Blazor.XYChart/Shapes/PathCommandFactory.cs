08ac777a74c0bf8eee045f921453e393b4f46b53
Maikel Bos <maikel.bos0@gmail.com>
2024-05-20 09:54:03 +0200

    Add bezier path methods

--- src/VDT.Core.Blazor.XYChart/Shapes/PathCommandFactory.cs
+++ src/VDT.Core.Blazor.XYChart/Shapes/PathCommandFactory.cs
@@ -26,4 +26,26 @@ public static class PathCommandFactory {
     /// <param name="y">Absolute Y-coordinate</param>
     /// <returns>The created line command</returns>
     public static string LineTo(decimal x, decimal y) => FormattableString.Invariant($"L {x} {y}");
+
+    /// <summary>
+    /// Create a command to draw a quadratic bezier curve via a control point to the provided absolute coordinates
+    /// </summary>
+    /// <param name="x1">Absolute X-coordinate of the control point</param>
+    /// <param name="y1">Absolute Y-coordinate of the control point</param>
+    /// <param name="x">Absolute X-coordinate</param>
+    /// <param name="y">Absolute Y-coordinate</param>
+    /// <returns>The created line command</returns>
+    public static string QuadraticBezierTo(decimal x1, decimal y1, decimal x, decimal y) => FormattableString.Invariant($"Q {x1} {y1}, {x} {y}");
+
+    /// <summary>
+    /// Create a command to draw a cubic bezier curve via two control points to the provided absolute coordinates
+    /// </summary>
+    /// <param name="x1">Absolute X-coordinate of the first control point</param>
+    /// <param name="y1">Absolute Y-coordinate of the first control point</param>
+    /// <param name="x2">Absolute X-coordinate of the second control point</param>
+    /// <param name="y2">Absolute Y-coordinate of the second control point</param>
+    /// <param name="x">Absolute X-coordinate</param>
+    /// <param name="y">Absolute Y-coordinate</param>
+    /// <returns>The created line command</returns>
+    public static string CubicBezierTo(decimal x1, decimal y1, decimal x2, decimal y2, decimal x, decimal y) => FormattableString.Invariant($"C {x1} {y1}, {x2} {y2}, {x} {y}");
 }
